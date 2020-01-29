5cbf187ab77e60348bd193b91d1265c31c013440
felipegontijo <epilef.gontijo@gmail.com>
2020-01-29 17:38:00 -0600

    Establish connection with Mail Server

--- SMTP_PA.py
+++ SMTP_PA.py
@@ -4,3 +4,37 @@ from socket import *
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+
+'''
+@pre    clientSocket created.
+@post   email sent and connection ended.
+    Starts the connection with mailserver.
+    Starts the secure connection with mailserver.
+    Goes through the whole process of sending the email, from authenticating the user
+    to ending the connection after the email has been sent.
+@param  clientSocket: socket object
+@param  mailserver: string
+'''
+def openSocket(clientSocket, mailserver):
+
+    clientSocket.connect((mailserver, 587))
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
+    if recv[:3] != '250':
+        print('250 reply not received from server.')
+
+    sendToServer(clientSocket, 'HELO outlook.com\r\n', '250')
+
+    sendToServer(clientSocket, 'starttls\r\n', '220')
+
+    secureClientSocket = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_SSLv23)
+
+    sendSecure(secureClientSocket, 'HELO outlook.com\r\n', '250')
+
+    authenticate(secureClientSocket)
+
+    msgString = writeMail(secureClientSocket)
+
+    sendMail(secureClientSocket, msgString)
+
+    endSecureConnection(secureClientSocket)
